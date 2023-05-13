751022a0efe1a36de007a4f7ab3ea5e53432ddab
Mr E <98057564+ExtremeMalmz@users.noreply.github.com>
2023-05-13 13:06:01 +0200

    added the checkers for the data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
+//import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  //this is where the REAL JS code is
+  
+  //checks if input is correct
+  function check_if_input_is_correct(){
+    //movie title from the input
+    var movieTitle = document.getElementById("inputTitle").value;
+    //movie score from the drop down list
+    var movieScore = document.getElementById("betyg-menu").value;
+    
+    //if you want to print the inputs
+    //console.log(movieTitle);
+    //console.log(movieScore);
+
+    //if title is empty or moviescore is option0 then inputs are wrong
+    if(movieTitle === "" || movieScore === "option0"){
+      return false;
+    }
+    //if all is right then we do this
+    else if (movieTitle !== "" && movieScore !== "option0"){
+      return true;
+    }
+    //double checking with an else
+    else{
+      return false;
+    }
+  }
+
+  //adds an item
+  function addItem(){
+    var areInputsValid = check_if_input_is_correct();
+
+    if(areInputsValid){
+      console.log("we gon add em");
+    }
+    else if(!areInputsValid){
+      console.log("yell at the user");
+    }
+    else{
+      console.log("How did we end up here?")
+    }
+  }
+
+  //this the HTML 
   return (
     <div className="App">
       <h1>Erics ToDo List</h1>
@@ -8,7 +52,7 @@ function App() {
       <hr></hr>
 
       <p>Titel:</p>
-      <input type ="text" placeholder = "Lägg till titel"></input>
+      <input type ="text" placeholder = "Lägg till titel" id = "inputTitle"></input>
 
       <p>Betyg:</p>
       <select id="betyg-menu">
@@ -21,8 +65,9 @@ function App() {
       </select>
 
       <br></br>
-      <button>Adda din film!</button>
+      <button onClick={doThisStuff => addItem()}>Adda din film!</button>
 
+      <ul></ul>
     </div>
   );
 }
